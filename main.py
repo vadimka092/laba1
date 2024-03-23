4d71096b39133a7bf62409c9cfe32fc7855ec2ec
vadimka092 <150704842+vadimka092@users.noreply.github.com>
2024-03-23 10:23:01 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,109 @@
+#Формируется матрица F следующим образом: если в В количество простых чисел в нечетных столбцах в области 2 больше, чем произведение чисел по периметру области 3, то поменять в В симметрично области 1 и 3 местами, иначе С и В поменять местами несимметрично. При этом матрица А не меняется. После чего вычисляется выражение: ((К*A T)*(F+А)-K* F T . Выводятся по мере формирования А, F и все матричные операции последовательно.
+
+
+import random
+
+def is_prime(num):
+    if num < 2:return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def create_matrix(N):
+    return [[random.randint(-10, 10) for _ in range(N)] for _ in range(N)]
+
+def count_primes_in_odd_columns(B):
+    count = 0
+    for row in B:
+        for i in range(0, len(row), 2):
+            if is_prime(row[i]):
+                count += 1
+    return count
+
+def product_of_perimeter_of_submatrix(matrix):
+    peri_product = 1
+    n = len(matrix)
+    m = len(matrix[0])
+    for i in range(n):
+        for j in range(m):
+            if i == 0 or i == n-1 or j == 0 or j == m-1:
+                peri_product *= matrix[i][j]
+    return peri_product
+
+def transpose_matrix(matrix):
+    return [list(row) for row in zip(*matrix)]
+
+def multiply_matrix_by_scalar(matrix, scalar):
+    return [[scalar * cell for cell in row] for row in matrix]
+
+def subtract_matrices(A, B):
+    return [[A[i][j] - B[i][j] for j in range(len(A[i]))] for i in range(len(A))]
+
+def matrix_multiplication(A, B):
+    result = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(' '.join(map(str, row)))
+    print()
+
+def main():
+    K = int(input("Введите число K: "))
+    N = int(input("Введите размерность матрицы N: "))
+
+    if N % 2 != 0:
+        print("N должно быть четным.")
+        return
+
+    A = create_matrix(N)
+    print("Матрица A:")
+    print_matrix(A)
+
+    mid = N // 2
+    B = [row[:mid] for row in A[:mid]]
+    C = [row[mid:] for row in A[:mid]]
+    D = [row[:mid] for row in A[mid:]]
+    E = [row[mid:] for row in A[mid:]]
+
+    if count_primes_in_odd_columns(B) > product_of_perimeter_of_submatrix(E):
+        for i in range(mid):
+            for j in range(mid):
+                E[i][j], E[j][i] = E[j][i], E[i][j]
+    else:
+        C, E = E, C
+
+    F = [B[i] + C[i] for i in range(mid)] + [D[i] + E[i] for i in range(mid)]
+
+    print("Матрица F после преобразований:")
+    print_matrix(F)
+
+    KA = multiply_matrix_by_scalar(A, K)
+    A_transposed = transpose_matrix(A)
+    KA_transposed = multiply_matrix_by_scalar(A_transposed, K)
+    A_squared = matrix_multiplication(A, A)
+    KAA_minus_KAT = subtract_matrices(multiply_matrix_by_scalar(A_squared, K), KA_transposed)
+
+    # Вывод результатов
+    print("Матрица KA (K умноженное на A):")
+    print_matrix(KA)
+
+    print("Матрица A транспонированная:")
+    print_matrix(A_transposed)
+
+    print("Матрица KAT (K умноженное на A транспонированное):")
+    print_matrix(KA_transposed)
+
+    print("Матрица KAA (K умноженное на A квадрат):")
+    print_matrix(multiply_matrix_by_scalar(A_squared, K))
+
+    print("Результат выражения ((К*A)A– KA^T):")
+    print_matrix(KAA_minus_KAT)
+
+if __name__ == "__main__":
+    main()
